60f4bb8c0bd165ec742c48acc998d71e46ced995
Matt Mowris <matt.mowris@gmail.com>
2016-09-06 10:23:21 -0700

    new

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL;
-
+pg.defaults.ssl = true;ß
 var client = new pg.Client(connectionString);
 client.connect();
 var query = client.query;
