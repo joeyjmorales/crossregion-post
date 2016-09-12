cd59bc31823e7ab1ad97e72e6bd9cb4d47e754a0
Matt Mowris <matt.mowris@gmail.com>
2016-09-12 11:28:13 -0700

    does post work?

--- modules/objects/obj/object.js
+++ modules/objects/obj/object.js
@@ -1,7 +1,8 @@
 var _ = require("lodash"),
-    async = require("async"),
-    pg = require("pg");
-    request = require("superagent");
+async = require("async"),
+pg = require("pg");
+request = require("superagent"),
+superagent = require("request");
 
 pg.defaults.ssl = true;
 
@@ -12,12 +13,12 @@ var sharedPgClient;
 
 pg.connect(dbString, function(err,client){
 
-    if(err){
-        console.error("PG Connection Error")
-    }
+  if(err){
+    console.error("PG Connection Error")
+  }
 
-    console.log("Connected to Postgres");
-    sharedPgClient = client;
+  console.log("Connected to Postgres");
+  sharedPgClient = client;
 
 });
 
@@ -25,30 +26,41 @@ var object = function(params){};
 
 object.prototype.get = function(params, callback) {
 
-    sharedPgClient.query('SELECT * FROM tests', function(err, result) {
+  sharedPgClient.query('SELECT * FROM tests', function(err, result) {
 
         //if (err) throw err;
 
         for(var i=0;i<result.length;i++){
-            console.log(result.rows[i]);
+          console.log(result.rows[i]);
         }
         callback(err, JSON.stringify(result));
 
-    });
+      });
 };
 
 object.prototype.post = function(params, callback) {
 
-    /* POST writes to Master DB in West Coast Private Space -a oregon-db  */
-    request
-  .post('https://oregon-db.herokuapp.com/api/v1/test/' + params.inputString)
+  /* POST writes to Master DB in West Coast Private Space -a oregon-db  */
+
+    //request.get('http://oregon-db.com/api/v1/test/h')
+    superagent('http://oregon-db.com/api/v1/test/h', function (error, response, body) {
+      if(err) {
+        console.log("Error",err);
+      }
+
+      if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+});
+   /* superagent
+  .post('oregon-db.herokuapp.com/api/v1/test/' + params.inputString)
   .send({ public: true, body: params.commentText })
   .set('Accept', 'application/json')
   .end(function(err, res){
     if(err) {
         console.log("err",err);
     }
-  });
+  });*/
 };
 
 module.exports = object;
